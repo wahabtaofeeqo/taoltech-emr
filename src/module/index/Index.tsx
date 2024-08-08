7a097702fa673d84bcc1f8192f21fb2991d9174b
Wahab Taofeek <taofeekolamilekan218@yahoo.com>
2024-08-08 18:58:33 +0100

    Feat: router added

---
+++ src/module/index/Index.tsx
@@ -0,0 +1,9 @@
+const Index = () => {
+    return (
+        <>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione earum officiis consectetur! Perspiciatis animi sed doloremque fugit ut, consectetur veniam ab quod, rem cupiditate possimus, corporis unde inventore doloribus omnis!
+        </>
+    )
+}
+
+export default Index;

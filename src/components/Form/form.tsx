f6c39d36ff892fd0c634315a485c90340c4fa870
Bolaji oluwatobi <bolajioluwatobi249@gmail.com>
2024-08-09 19:05:13 +0100

    my unfinished task

---
+++ src/components/Form/form.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    dateOfBirth: '',
+    maritalStatus: '',
+    hmo: '',
+    occupation: ''
+  });
+
+  const handleChange = (e:any) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="firstName">First Name:</label>
+        <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name:</label>
+        <input type="text" id="lastName" name="lastName" value={formData.lastName} onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="dateOfBirth">Date of Birth:</label>
+        <input type="date" id="dateOfBirth" name="dateOfBirth" value={formData.dateOfBirth} onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="maritalStatus">Marital Status:</label>
+        <select id="maritalStatus" name="maritalStatus" value={formData.maritalStatus} onChange={handleChange}>
+          <option value="">Select</option>
+          <option value="single">Single</option>
+          <option value="married">Married</option>
+          <option value="divorced">Divorced</option>
+          <option value="widowed">Widowed</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="hmo">HMO:</label>
+        <input type="text" id="hmo" name="hmo" value={formData.hmo} onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="occupation">Occupation:</label>
+        <input type="text" id="occupation" name="occupation" value={formData.occupation} onChange={handleChange} />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;

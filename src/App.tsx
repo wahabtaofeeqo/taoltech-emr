c4b8eed55ce8908c4914a2f18ea359518d29939a
Wahab Taofeek <taofeekolamilekan218@yahoo.com>
2024-08-18 12:23:28 +0100

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from './components/layouts/layout';
 import Index from './module/index/Index';
+import Resource from './module/doctor/resource';
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
+    <BrowserRouter>
         <Routes>
           <Route path='' element={<Index />} />
-          <Route path="/dashboatd" element={<Layout />}>
+          <Route path="/dashboard" element={<Layout />}>
+            {/* Inner routes should be here */}
+            <Route path='resource' element={<Resource />} />
           </Route>
         </Routes>
     </BrowserRouter>
---
+++ src/module/doctor/resource.tsx
@@ -0,0 +1,9 @@
+const Resource = () => {
+    return (
+        <>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus in ullam dolores itaque qui quia maxime? Quisquam, quod? Debitis, error nihil! Sapiente quibusdam repellat ut unde temporibus tenetur necessitatibus quo?
+        </>
+    )
+}
+
+export default Resource;
